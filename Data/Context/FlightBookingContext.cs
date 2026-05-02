e13c54a37912a600d4e9687392984a837873e881
Ali Bakhtiari Moghaddam <bakhtiarimoghadam1383@gmail.com>
2026-05-02 22:52:32 +0330

    initial Project

---
+++ Data/Context/FlightBookingContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class FlightBookingContext:DbContext
+    {
+        #region Constructor
+
+        public FlightBookingContext(DbContextOptions<FlightBookingContext> options) : base (options)
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ FlightBooking.API/Program.cs
@@ -0,0 +1,40 @@
+using Data.Context;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+#region AddDbContext
+
+builder.Services.AddDbContext<FlightBookingContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+#endregion
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
